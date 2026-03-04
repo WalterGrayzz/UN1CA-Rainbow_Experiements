ebeb3bfdc9506a8a2fe80d5da838fde6e976a10d
Walter Grayz <alenh944@gmail.com>
2026-03-04 13:30:31 +0400

    fishe

--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -24,3 +24,10 @@ DELETE_FROM_WORK_DIR "system" "system/bin/qchdcpkprov"
 DELETE_FROM_WORK_DIR "system" "system/bin/dhkprov"
 DELETE_FROM_WORK_DIR "system" "system/etc/init/dhkprov.rc"
 LOG_STEP_OUT
+
+#Soundbooster
+LOG_STEP_IN "- Replacing SoundBooster"
+DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SoundBooster_ver2000.so"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/lib_SoundBooster_ver1100.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
+LOG_STEP_OUT
