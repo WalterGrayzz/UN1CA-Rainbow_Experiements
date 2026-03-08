6e18abe432c0f725d6fb717e1b0ce8eb6b0980c1
Walter Grayz <alenh944@gmail.com>
2026-03-08 13:53:23 +0400

    idk

--- unica/patches/Hotward/customize.sh
+++
@@ -1,6 +0,0 @@
-LOG_STEP_IN "- Replacing Hotword"
-    DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentXGoogleEx4HEXAGON"
-    DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentOKGoogleEx4HEXAGON"
-    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "priv-app/HotwordEnrollmentOKGoogleEx4HEXAGON/HotwordEnrollmentOKGoogleEx4HEXAGON.apk" 0 0 644 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "priv-app/HotwordEnrollmentOKGoogleEx4HEXAGON/HotwordEnrollmentOKGoogleEx4HEXAGON.apk" 0 0 644 "u:object_r:system_file:s0"
-LOG_STEP_OUT
--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -1,4 +1,3 @@
-SET_PROP_IF_DIFF "vendor" "ro.oem_unlock_supported" "0"
 
 LOG "- Disabling encryption"
 LINE=$(sed -n "/^\/dev\/block\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.qcom")
