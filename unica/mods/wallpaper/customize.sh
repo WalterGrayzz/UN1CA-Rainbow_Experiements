ca1f743f4c7fc500fc30d9b073cf8dc2bc0620eb
Walter Grayz <alenh944@gmail.com>
2026-04-23 17:00:20 +0400

    s

--- unica/mods/wallpaper/customize.sh
+++
@@ -1,5 +0,0 @@
-
-ADD_TO_WORK_DIR "m3qxxx" "system" \
-    "system/priv-app/wallpaper-res/wallpaper-res.apk" 0 0 644 "u:object_r:system_file:s0"
-ADD_TO_WORK_DIR "m3qxxx" "system" \
-    "system/priv-app/SpriteWallpaper/SpriteWallpaper.apk" 0 0 644 "u:object_r:system_file:s0"
--- unica/patches/vulkan/customize.sh
+++
@@ -1,7 +0,0 @@
-
-LOG " - Enabling vulkan"
-SET_PROP "vendor" "ro.hwui.use_vulkan" "true"
-SET_PROP "vendor" "debug.hwui.use_hint_manager" "true"
-SET_PROP "system" "debug.hwui.renderer" "skiavk"
-SET_PROP "system" "debug.hwui.skip_empty_frames" "1"
-SET_PROP "system" "ro.hwui.use_vulkan" "true"
